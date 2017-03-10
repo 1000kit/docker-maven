5d13226f372239541e5eda86f33d8e386f4ca53d
tasgit <tasgit@1000kit.org>
2017-03-10 15:55:38 +0100

    move copy to create

--- init/createSettings.sh
+++ init/createSettings.sh
@@ -2,5 +2,13 @@
 
   cd /opt
   envsubst < settings_template.xml > settings.xml
+
+
+  if [ ! -d ~/.m2/ ]; then
+     mkdir -p ~/.m2/
+  fi
+  cp settings.xml settings-security.xml ~/.m2/
   
+  
+
 #end
--- init/entrypoint.sh
+++ init/entrypoint.sh
@@ -4,12 +4,6 @@ cd /opt
 chmod 755 createSettings.sh
 ./createSettings.sh
 
-if [ ! -d /root/.m2/ ]; then
-  mkdir -p /root/.m2/
-fi
-cp settings.xml settings-security.xml /root/.m2/
-
-
 ## start entry point
 /usr/local/bin/mvn-entrypoint.sh $@
 
